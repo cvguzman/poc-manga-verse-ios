cffa3abbd0abf1f021dae928b49d3a6fea979d69
Cinthya Guzmán <cinthya.dev@icloud.com>
2024-06-10 19:56:23 -0400

    First commit

---
+++ MangaVerse/MangaVerseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangaVerseApp.swift
+//  MangaVerse
+//
+//  Created by Cynthia Guzman on 10-06-24.
+//
+
+import SwiftUI
+
+@main
+struct MangaVerseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
