2548ddc67c036b50b31421d11535071882d2752d
Cinthya Guzmán <cinthya.dev@icloud.com>
2024-07-13 20:46:05 -0400

    manga data layer now adds search by categories and reduced boilerplate

--- MangaVerse/Core/DataSources/Endpoint.swift
+++ MangaVerse/Core/DataSources/Endpoint.swift
@@ -1,8 +1,16 @@
 enum Endpoint {
     enum Manga: String {
+        // MARK: - General
         case paginatedList = "/list/mangas?page=%@&per=20"
         case genres = "/list/genres"
         case demographics = "/list/demographics"
         case themes = "/list/themes"
+        
+        // MARK: - Search
+        case searchByWord = "/search/mangasContains/%@"
+        case searchByGenre = "/list/mangaByGenre/%@"
+        case searchByDemographic = "/list/mangaByDemographic/%@"
+        case searchByTheme = "/list/mangaByTheme/%@"
+        case searchByAuthor = "/list/mangaByAuthor/%@?page=1&per=20"
     }
 }
