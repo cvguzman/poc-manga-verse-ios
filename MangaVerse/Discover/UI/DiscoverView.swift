cfb3a8e9a7ae69c3a0cef23fbe2ddb856bb5ac90
Cinthya Guzmán <cinthya.dev@icloud.com>
2024-07-15 22:18:49 -0400

    first concept draft  of DiscoverView

--- MangaVerse/Discover/UI/DiscoverView.swift
+++ MangaVerse/Discover/UI/DiscoverView.swift
@@ -2,7 +2,39 @@ import SwiftUI
 
 struct DiscoverView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView(content: {
+            List {
+                NavigationLink {
+                    EmptyView()
+                } label: {
+                    Button(action: {}, label: {
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text("Monster")
+                                    .font(.title)
+                                    .bold()
+                                Text("Sub")
+                                    .font(.title2)
+                            }
+                            .foregroundStyle(.white)
+                            Spacer()
+                        }
+                        .padding()
+                    })
+                }
+                .background(
+                    ZStack {
+                        AsyncImage(url: URL(string: "https://cdn.myanimelist.net/images/manga/1/157897l.jpg"), scale: 1.6)
+                            .scaledToFill()
+                            .opacity(0.6)
+                        LinearGradient(gradient: Gradient(colors: [.black, .gray, .white, .white]), startPoint: .leading, endPoint: .trailing)
+                            .ignoresSafeArea()
+                            .opacity(0.8)
+                    }
+                )
+            }
+            .navigationTitle("Discover")
+        })
     }
 }
 
