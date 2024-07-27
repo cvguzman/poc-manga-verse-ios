48cd0863e377b39c16d206d75881144d5c6d3069
Cinthya Guzmán <cinthya.dev@icloud.com>
2024-07-27 15:05:43 -0400

    NavigationView was migrated into NavigationStack

--- MangaVerse/Discover/UI/DiscoverView.swift
+++ MangaVerse/Discover/UI/DiscoverView.swift
@@ -4,24 +4,25 @@ struct DiscoverView: View {
     @Environment(DiscoverViewModel.self) private var viewModel
 
     var body: some View {
-        NavigationView(content: {
+        NavigationStack {
             ScrollView {
                 LazyVGrid(columns:[GridItem(.adaptive(minimum: 120))], content: {
                     ForEach(viewModel.mangas) { manga in
-                        NavigationLink {
-                            EmptyView()
-                        } label: {
+                        NavigationLink(value: manga) {
                             CardView(model: manga)
                         }
                     }
                 })
                 .padding(.horizontal)
             }
+            .navigationDestination(for: MangaEntity.self, destination: { manga in
+                MangaDetailView(manga: manga)
+            })
             .navigationTitle("Discover")
             .task {
                 await viewModel.fetchMangas()
             }
-        })
+        }
     }
 }
 
