0f2e54acb178944c1765aefffccab30512d8fd7e
Cinthya Guzmán <cinthya.dev@icloud.com>
2024-08-21 23:26:33 -0400

    DiscoverView now can search mangas by name or any word

--- MangaVerse/Presentation/Discover/DiscoverView.swift
+++ MangaVerse/Presentation/Discover/DiscoverView.swift
@@ -2,22 +2,29 @@ import SwiftUI
 
 struct DiscoverView: View {
     let environment: MangaVerseEnvironment
+    @State private var searchText = String()
+    @State private var readyForSearch = false
 
     var body: some View {
         NavigationStack {
-            PaginatedListView(viewModel: environment.paginatedListViewModel, loaderType: .byPage)
-                .navigationTitle("Discover")
-                .toolbar {
-                    ToolbarItem(placement: .topBarTrailing) {
-                        NavigationLink {
-                            EmptyView()
-                                .navigationTitle("Search")
-                        } label: {
-                            Image(systemName: "magnifyingglass.circle.fill")
-                                .tint(.primary)
-                        }
-                    }
+            Group {
+                if !readyForSearch {
+                    PaginatedListView(viewModel: environment.paginatedListViewModel, loaderType: .byPage)
+                } else {
+                    PaginatedListView(viewModel: environment.paginatedListViewModel, loaderType: .byMatchingWord(searchText))
                 }
+            }
+            .navigationTitle("Discover")
+            .searchable(
+                text: $searchText,
+                placement: .navigationBarDrawer(displayMode: .always),
+                prompt: "Search by name or any word")
+            .onChange(of: searchText, {
+                readyForSearch = false
+            })
+            .onSubmit(of: .search) {
+                readyForSearch = true
+            }
         }
     }
 }
